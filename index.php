869d1d85285e42e8573277f9aa1f5be9a2a79747
Life <99041864+dalife001@users.noreply.github.com>
2023-01-31 20:21:26 +0200

    Just been
    just being doing it

---
+++ index.php
@@ -0,0 +1,12 @@
+ require 'Legal.html';
+<div class="right-menu">
+    <nav>
+        <a href="#">Menu1</a>
+        <a href="#">Menu2</a>
+        <a href="#">Menu3</a>
+    </nav>
+</div>
+
+<div class="left-signin">
+    <a href="#">Sign In</a>
+</div>
